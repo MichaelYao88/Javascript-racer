121eac037bf52e87cb79177f16dc40714051235a
Anne Spalding <aespaldi@gmail.com>
2014-01-23 13:07:57 -0800

    format and releases

--- ar-skeleton/config/application.rb
+++ ar-skeleton/config/application.rb
@@ -3,6 +3,9 @@ require 'sqlite3'
 require 'active_record'
 require 'logger'
 
+
+## Load all files and configure the db
+
 APP_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
 
 APP_NAME = APP_ROOT.basename.to_s
@@ -13,10 +16,6 @@ if ENV['DEBUG']
   ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
 
-# Automatically load every file in APP_ROOT/app/models/*.rb, e.g.,
-#   autoload "Person", 'app/models/person.rb'
-#
-# See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
 
 Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
   filename = File.basename(model_file).gsub('.rb', '')
