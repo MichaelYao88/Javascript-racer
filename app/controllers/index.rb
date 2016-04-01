344e96369ae4a48d438b5bee1284e5587626c5b2
Michael Yao <email": null>
2016-04-01 17:24:54 -0500

    finish

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,3 @@
 get '/' do
-  redirect '/horses'
+  erb :'racer/index'
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,61 +1,78 @@
-// Ajax is like this thing in javascropt that's all like yo I gonna send an http request based on some specifications like type, data, url, etc. When the request finishes usually we get back a string or json object and we usually do some stuff with it
-
 $(document).ready(function() {
-  // Creates Horse Form
-  $("#new-horse").on("click",function(event){
-    event.preventDefault();
-    var url= $(this).attr('href');
-    var $that = $(this);
-
-    var request = $.ajax({
-      type: 'GET',
-      url: url
-    });
-
-    request.done(function(data){
-      $('p').append(data);
-    });
-  });
-// Creates New Horse off of created form
-  $("p").on("submit","#horse-form",function(event){
-    console.log("yo")
-    event.preventDefault();
-    var action = $(this).attr('action');
-    var $that = $(this);
-    var data = $("input[type=text]").serialize()
 
 
-    var request = $.ajax({
-      type: 'POST',
-      url: action,
-      data: data
-    });
+var $finished=0
+var $player1=0
+var $player2=0
+  $(document).on('keyup', function(key) {
+    if($finished === 0){
+      if(key.which === 65){$finished = update_player1_position();$player1+=1};
+      if(key.which === 76){$finished = update_player2_position();$player2+=1};
+    }
+    if($finished === 1){
+      $(".player1_score").text("Player 1 presses: "+$player1);
+      $(".player2_score").text("Player 2 presses: "+$player2);
+      $(".game").text("Press X to restart");
+    }
+    if(key.which === 88){restart_position(); $finished=0; $player1=0;
+      $player2=0};
 
-    request.done(function(response){
-      $('#horse-list').append(response)
-      $('#horse-form').remove()
-    });
 
-  });
-
-// Shows Horse Stats/Removes Horse Stats
-$("#horse-list").on("click","a",function(event){
-    event.preventDefault();
-    var url= $(this).attr('href');
-    var $that = $(this);
-
-    var request = $.ajax({
-      type: 'GET',
-      url: url,
-    });
-
-    request.done(function(response){
-      $(".stats").remove();
-      $that.closest('div').append(response)
-    });
   });
 
 
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var update_player1_position = function(){
+  var advance = $("#player1_strip").find(".active").index()+Math.floor(Math.random() * 4);
+  var win = 0;
+  if(advance >=28) {advance = 28; win = 1};
+  var advance = "#player1_strip td:nth-child("+advance+")";
+
+  $(".active").removeClass();
+  $(advance).addClass("active")
+  return win
+};
+
+ var update_player2_position = function(){
+  var advance = $("#player2_strip").find(".active2").index()+Math.floor(Math.random() * 4);
+  var win = 0;
+  if(advance >=28) {advance = 28; win = 1};
+  var advance = "#player2_strip td:nth-child("+advance+")";
+  $(".active2").removeClass();
+  $(advance).addClass("active2")
+  return win
+};
+
+var restart_position = function(){
+  $(".active2").removeClass();
+  $(".active").removeClass();
+  $("#player1_strip td").first().addClass("active");
+  $("#player2_strip td").first().addClass("active2");
+  $(".player1_score").text("")
+  $(".player2_score").text("")
+  $(".game").text("")
+
+
+};
+
